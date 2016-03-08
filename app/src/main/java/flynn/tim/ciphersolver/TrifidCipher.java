1c93cc2aa3ceb1690bf63de75cbb32eabe417522
pcummuskey <pcummuskey@bravurasolutions.com>
2016-03-09 11:21:16 +1300

    Creating file for dealing with trifid cipher.

---
+++ app/src/main/java/flynn/tim/ciphersolver/TrifidCipher.java
@@ -0,0 +1,22 @@
+package flynn.tim.ciphersolver;
+
+import java.io.IOException;
+
+/**
+ * Implementation of the Trifid cypher.
+ * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
+ */
+public class TrifidCipher {
+
+	public static void main(String[] args) throws IOException {
+	}
+
+	public static String decode(String encoded) {
+		return null;
+	}
+
+	public static String encode(String decoded) {
+		return null;
+	}
+
+}
